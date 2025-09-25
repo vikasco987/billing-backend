ba9af7d28ecdf08c362378d0dfc5dce34589c2a9
vikasco987 <vikasvikas9095@gmail.com>
2025-09-25 16:28:14 +0530

    add

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+// eslint.config.js
+import { FlatCompat } from '@eslint/eslintrc-compat';
+
+const compat = new FlatCompat({
+  baseDirectory: process.cwd(),
+});
+
+export default [
+  ...compat.extends('next/core-web-vitals'),
+
+  {
+    files: ['**/*.{ts,tsx}'],
+    ignores: ['src/generated/prisma/**'], // ignore Prisma generated files
+    languageOptions: {
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2024,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+        project: './tsconfig.json', // needed if using type-aware linting
+      },
+    },
+    plugins: ['@typescript-eslint'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-expressions': 'off',
+      '@typescript-eslint/no-this-alias': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+    },
+  },
+];
