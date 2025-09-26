1736c737c9703ca8f5b5a110cb99977ca93f96a9
vikasco987 <vikasvikas9095@gmail.com>
2025-09-26 16:07:52 +0530

    add

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true, // Keep your previous setting
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**", // Allow all images from Cloudinary
+      },
+    ],
   },
 };
 
